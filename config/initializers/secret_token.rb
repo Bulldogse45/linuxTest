6e26aef87bdb3bd01d1c6aa06abd1fe869e96a4e
Matt F <matt.j.fair@gmail.com>
2016-01-18 23:27:35 -0500

    just adding some stuff on my ubuntu machine to see if I can push it all up to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinuxTest::Application.config.secret_token = 'eaf07faa8ac25a1241039f5ed86050d3cd3efc203b2828a8b95d13695dd96ee0c72c820397b04f5ea3d6e29caa1298fe4fd2ea3a8e740df4235d510355772c4d'
